d9f1e86d4e694f3d0d145599996bf664dd9d018f
dawnsoapp <madi.jackson29@gmail.com>
2024-03-12 13:26:33 -0700

    Adding changes to Testing file

---
+++ Testing.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int myNum()
+{
+    int x = 5;
+    int y = 6;
+    int result = x + y;
+    cout<< result;
+    return result;
+}
+
+int main()
+{
+    cout<< "Hello World. My name is Madi. \n";
+    cout<< "5 + 6 is: ", myNum();
+}
